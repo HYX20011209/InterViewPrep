5f1fd2fc967c72165addf436cf3999eccf5b5a63
HYX20011209 <spacetime57@163.com>
2025-06-02 21:15:05 +0800

    finish basic backend functions

---
+++ InterViewPrep-backend/src/main/java/com/hyx/InterViewPrep/mapper/QuestionBankQuestionMapper.java
@@ -0,0 +1,18 @@
+package com.hyx.InterViewPrep.mapper;
+
+import com.hyx.InterViewPrep.model.entity.QuestionBankQuestion;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author hyx
+* @description 针对表【question_bank_question(题库题目)】的数据库操作Mapper
+* @createDate 2025-06-01 17:00:53
+* @Entity com.hyx.InterViewPrep.model.entity.QuestionBankQuestion
+*/
+public interface QuestionBankQuestionMapper extends BaseMapper<QuestionBankQuestion> {
+
+}
+
+
+
+
